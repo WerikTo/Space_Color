cea5e67293b2cf0d2bc194accf0df88d1d32def8
ylocinn <115472908+ylocinn@users.noreply.github.com>
2023-08-15 13:47:21 -0300

    Update spacecolor.js

--- spacecolor.js
+++ spacecolor.js
@@ -1,1 +1,18 @@
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+pendown = false;
+canvas.onmousedown = function(){
+    pendown = true;
+}
+canvas.onmouseup = function(){
+    pendown = false;
+}
+
+
+canvas.onmousemove = function(e){
+    if(pendown){
+        ctx.fillStyle = "#000";
+        ctx.fillRect(e.pageX- 10, e.pageY- 5, 4, 4);
+    }
+}
 
